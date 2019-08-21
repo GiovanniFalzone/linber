191605434ba67e6d6f7ea740125040aaa8626f9f
Giovanni <falzone.giovanni2@gmail.com>
2019-08-21 13:20:31 +0200

    makemodule option

--- main.sh
+++ main.sh
@@ -1,26 +1,38 @@
 #!/bin/sh
 cmd="${1}" 
 case ${cmd} in 
+	makemodule)
+		sh -c 'cd driver && make clean && make all'
+		;;
+
+	maketest)
+		sh -c 'cd test && make clean && make all'
+		;;
+
 	makeall)
 		sh -c 'cd driver && make clean && make all'
 		sh -c 'cd test && make clean && make all'
 		;;  
+
 	clean)
 		sh -c 'cd driver && make clean'
 		sh -c 'cd test && make clean'
 		;; 
+
 	insmod)
 		sh -c 'sudo insmod driver/linber.ko'
 		;; 
+
 	rmmod)
 		sh -c 'sudo rmmod linber'
 		;; 
+
 	updatemod)
-		sh -c 'cd driver && make && sudo rmmod linber && sudo insmod linber.ko'
+		sh -c 'cd driver && make all && sudo rmmod linber && sudo insmod linber.ko'
 		;;
 
    *)  
-	  echo "`basename ${0}`:usage: [makeall] | [clean] | [insmod] | [rmmod] | [updatemod]" 
+	  echo "`basename ${0}`:usage: [makeall] | [makemodule] | [maketest] | [clean] | [insmod] | [rmmod] | [updatemod]" 
 	  exit 1
 	  ;; 
 esac
