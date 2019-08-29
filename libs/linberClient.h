eddd73827fcff468716c70a1b477272d67aa7470
Giovanni <falzone.giovanni2@gmail.com>
2019-08-29 10:20:09 +0200

    request in shared memory

--- libs/linberClient.h
+++ libs/linberClient.h
@@ -14,10 +14,11 @@ class linberClient {
 	int response_len;
 	char* request = NULL;
 	char * response = NULL;
+	key_t request_shm_key;
 	boolean response_shm_mode = FALSE;
 	boolean request_shm_mode = FALSE;
 	unsigned long token;
-	int request_step = 0;
+	int request_state = 0, request_shm_state = -1;
 
 	public:
 	linberClient(char * service_uri, int service_uri_len){
@@ -32,33 +33,80 @@ class linberClient {
 
 	void linber_sendRequest(char *req, int req_len, char **res, int *res_len, bool blocking){
 		int ret;
-		this->request = req;
-		this->request_len = req_len;
-		if(request_step == 0){
+		if((request_state == 0) && (request_shm_state == -1)){
+			this->request_shm_mode = FALSE;
+			this->request = req;
+			this->request_len = req_len;
 			if(blocking){
 				ret = linber_request_service(	service_uri, service_uri_len,		\
 										10, request, request_len,					\
 										&response, &response_len, &response_shm_mode);
 				*res = response;
 				*res_len = response_len;
+				request_state = 2;
 			} else {
 				ret = linber_request_service_no_blocking(	service_uri, service_uri_len,	\
 															10, request, request_len,		\
 															&token);
+				request_state = 1;
 			}
 			if(ret < 0){
 				printf("request failed\n");
 				return;
 			}
-			request_step = 1;
 		} else {
 			printf("Only one request at time\n");
 		}
 	}
 
+
+	void linber_create_shm(char **req, int req_len){
+		key_t shm_key;
+		int shm_id;
+
+		if((request_state == 0) && (request_shm_state == -1)){
+			*req = create_shm_from_filepath(".", req_len, &shm_key, &shm_id);
+			if(*req == NULL){
+				printf("Error in shared memory allocation\n");
+				return;
+			}
+			this->request_shm_mode = TRUE;
+			this->request = *req;
+			this->request_len = req_len;
+			this->request_shm_key = shm_key;
+			request_shm_state = 0;
+		}
+	}
+
+	void linber_sendRequest_shm(char **res, int *res_len, bool blocking){
+		unsigned long token;
+		int ret, response_len;
+		char *response;
+		boolean response_shm_mode;
+
+		if(request_shm_state == 0){
+			if(blocking){
+				ret = linber_request_service_shm(	service_uri, service_uri_len,					\
+												10, request_shm_key, request_len,					\
+												&response, &response_len, &response_shm_mode);
+				*res = response;
+				*res_len = response_len;
+				request_shm_state = 2;
+			} else {
+				ret = linber_request_service_no_blocking_shm(	service_uri, service_uri_len,		\
+																10, request_shm_key, request_len,	\
+																&token);
+				request_shm_state = 1;
+			}
+		}
+		if(ret < 0){
+			printf("request failed\n");
+		}
+	}
+
 	void linber_get_result(){
 		int ret;
-		if(request_step == 1){
+		if((request_state == 1) || (request_shm_state == 1)){
 			ret = linber_request_service_get_response(	service_uri, service_uri_len,					\
 														&response, &response_len, &response_shm_mode,	\
 														&token);
@@ -66,23 +114,29 @@ class linberClient {
 				printf("get result failed\n");
 				return;
 			}
-			request_step = 2;
+			if(request_state == 1){
+				request_state = 2;
+			} else {
+				request_shm_state = 2;
+			}
 		} else {
 			printf("no request have been sent\n");
 		}
 	}
 
 	void linber_end_operation(){
-		if(request_step < 2){	// only request
+		if((request_state == 1) || (request_shm_state == 0 || request_shm_state == 1)){	// only request
 			response = NULL;
 			linber_request_service_clean(request, request_shm_mode, response, response_shm_mode);
-			request_step = 0;
-		} else if(request_step == 2){	// completed
+			request = NULL;
+			request_state = 0;
+			request_shm_state = -1;
+		} else if((request_state == 2) || (request_shm_state == 2)){	// completed
 			linber_request_service_clean(request, request_shm_mode, response, response_shm_mode);
 			response = NULL;
 			request = NULL;
-			request_step = 0;
-
+			request_state = 0;
+			request_shm_state = -1;
 		}
 	}
 };
